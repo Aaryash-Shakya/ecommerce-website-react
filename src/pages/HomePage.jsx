c75c40a6433ea6b679219cef408e45f2cfa89e5a
Aaryash-Shakya <100826663+Aaryash-Shakya@users.noreply.github.com>
2023-06-13 16:58:00 +0545

    effect, datafetch, fakestoreapi map

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import Carousel from '../components/Carousel'
 import Cards from '../components/Cards'
 import IncrementDecrement from '../hooks/IncrementDecrement'
+import Effect from '../hooks/Effect'
+import Datafetch from '../components/Datafetch'
 
 const HomePage = () => {
   return (
     <>
         <Carousel/>
         <Cards/>
-        <IncrementDecrement/>
+        {/* <IncrementDecrement/>
+        <Effect/> */}
+        <Datafetch/>
     </>
   )
 }
