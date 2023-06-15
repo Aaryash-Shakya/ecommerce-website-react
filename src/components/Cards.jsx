98a964c9e97232ddc550c0275410f8a6a161246d
Aaryash-Shakya <100826663+Aaryash-Shakya@users.noreply.github.com>
2023-06-15 11:56:34 +0545

    product mapping

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,45 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Cards = () => {
+//  note to pass data from parent component to child componenet use props
+const Cards = (props) => {
+  // const title = props.item.title
+  // const price = props.item.price
+  // note object destructuring
+  const{title,image,price,id}=props.item
+  // ! hw
+  // const productDetails = () =>{
+  //   props.item.map((props.item)=>{
+
+  //   })
+  // }
+
   return (
-    <div className="container-fluid py-3">
-      <div className="row row-cols-1 row-cols-md-4 g-4">
-        <div className="col">
-          <div className="card">
-            <img src="/img/1.jpg" className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <Link to='#' className="btn btn-primary">View Details</Link>
-            </div>
-          </div>
-        </div>  
-        <div className="col">
-          <div className="card">
-            <img src="/img/2.jpg" className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <Link to='#' className="btn btn-primary">View Details</Link>
-            </div>
-          </div>
-        </div>
-        <div className="col">
-          <div className="card">
-            <img src="/img/3.jpg" className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <Link to='#' className="btn btn-primary">View Details</Link>
-            </div>
-          </div>
-        </div>
-        <div className="col">
-          <div className="card">
-            <img src="/img/1.jpg" className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <Link to='#' className="btn btn-primary">View Details</Link>
-            </div>
-          </div>
+    <div className="col">
+      <div className="card">
+        <img src={image} className="card-img-top" alt={title} />
+        <div className="card-body">
+          <h5 className="card-title">Rs {price}</h5>
+          <Link to="#" className="btn btn-primary">
+            View Details
+          </Link>
         </div>
       </div>
     </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import Carousel from '../components/Carousel'
-import Cards from '../components/Cards'
 import IncrementDecrement from '../hooks/IncrementDecrement'
 import Effect from '../hooks/Effect'
 import Datafetch from '../components/Datafetch'
+import CardContainer from '../components/CardContainer'
 
 const HomePage = () => {
   return (
     <>
         <Carousel/>
-        <Cards/>
+        <CardContainer/>
         {/* <IncrementDecrement/>
         <Effect/> */}
-        <Datafetch/>
+        {/* <Datafetch/> */}
     </>
   )
 }
