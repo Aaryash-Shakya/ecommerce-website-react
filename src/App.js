2c06f5ce48c7502baea36490417bb4d21fb89ae0
Aaryash-Shakya <100826663+Aaryash-Shakya@users.noreply.github.com>
2023-06-30 19:09:01 +0545

    react redux payload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import MyRoutes from './MyRoutes';
-import { legacy_createStore } from 'redux';
+// import { legacy_createStore } from 'redux';
 // createStore ->used to tell compiler that a reducer is a store
 import { Provider } from 'react-redux';
 // Provider -> to pass data from reducer to component
-import cartReducer from './redux/reducers/cartReducer';
+// import cartReducer from './redux/reducers/cartReducer';
+import store from './store';
 
 const App = () => {
-  const store = legacy_createStore(cartReducer)
+  // const store = legacy_createStore(cartReducer)
   return (
     <>
       <Provider store={store}>
--- src/redux/TestCart.jsx
+++ src/redux/TestCart.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import TestItem from './TestItem'
+import Student from './Studnet'
 
 const TestCart = () => {
-    const data = useSelector(argStore=>argStore)
+    const data = useSelector(argStore=>argStore.cart)
   return (
     <>
     <h2>The total number of items in the care is {data.cartCount}</h2>
+
     <TestItem/>
+    <Student/>
     </>
   )
 }
