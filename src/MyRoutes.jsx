234c6d6ef1bbaf02503a7325838b8c3f60d3d48c
Aaryash-Shakya <100826663+Aaryash-Shakya@users.noreply.github.com>
2023-07-19 16:54:04 +0545

    email verify, signin, signup, authenticate, redirect

--- src/MyRoutes.jsx
+++ src/MyRoutes.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Layouts from './components/Layouts'
 import HomePage from './pages/HomePage'
 import TestNav from './TestNav'
@@ -10,22 +10,26 @@ import Cart from './pages/Cart'
 import Register from './pages/Register'
 import Show from './context/Show'
 import TestCart from './redux/TestCart'
+import EmailVerify from './auth/EmailVerify'
+import Profile from './pages/Profile'
 const MyRoutes = () => {
   return (
     <Router>
-        <Routes>
-          <Route path='' element={<Layouts/>}>
-            <Route index element={<HomePage/>}/>
-            <Route path='/test' element={<TestNav/>}/>
-            <Route path='/login' element={<Login/>}/>
-            <Route path='signup' element={<Register/>}/>
-            <Route path='/productdetails/:product_id' element={<ProductDetails/>}/>
-            <Route path='/product' element={<Product/>}/>
-            <Route path='cart' element={<Cart/>}/>  
-          </Route>
-          <Route path='context/api' element={<Show/>}/>
-          <Route path='redux/cart' element={<TestCart/>}/>
-        </Routes>
+      <Routes>
+        <Route path='' element={<Layouts />}>
+          <Route index element={<HomePage />} />
+          <Route path='/test' element={<TestNav />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='signup' element={<Register />} />
+          <Route path='profile' element={<Profile />} />
+          <Route path='email/confirmation/:token' element={<EmailVerify />} />
+          <Route path='/productdetails/:product_id' element={<ProductDetails />} />
+          <Route path='/product' element={<Product />} />
+          <Route path='cart' element={<Cart />} />
+        </Route>
+        <Route path='context/api' element={<Show />} />
+        <Route path='redux/cart' element={<TestCart />} />
+      </Routes>
     </Router>
   )
 }
